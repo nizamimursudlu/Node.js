6b251e8d8f8b75a40e69e5438cce6af914bb2aec
Nizami Mursudlu <nizamimursudlu@gmail.com>
2021-02-05 14:57:54 +0100

    exercise 1 is finished

--- week2/homework/exercises/1-blog-API/server.js
+++ week2/homework/exercises/1-blog-API/server.js
@@ -1,10 +1,53 @@
 const express = require('express')
 const app = express();
- 
+const fs = require("fs");
 
-// YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.use(express.json())
+
+
+app.post('/blogs', (req, res) => {
+  let title = req.body.title
+  let content = req.body.content
+  fs.writeFileSync(title, content)
+  res.end('ok')
 })
- 
+
+app.put('/posts/:title', (req, res) => {
+  let title = req.body.title
+  console.log(title)
+  let content = req.body.content
+  if (fs.existsSync(title) || fs.existsSync(content)) {
+    fs.writeFileSync(title, content);
+    res.end('ok')
+  }
+  else {
+    res.status(404)
+    res.send("This post does not exist!")
+  }
+})
+
+app.delete('/blogs/:title', (req, res) => {
+  let title = req.params.title
+  if (fs.existsSync(title)) {
+    fs.unlinkSync(title);
+    res.end('ok');
+  } else {
+    res.status(404)
+    res.send("This post does not exist!")
+  }
+})
+
+app.get('/blogs/:title', (req, res) => {
+  let title = req.params.title
+  if (fs.existsSync(title)) {
+    console.log(title)
+    const post = fs.readFileSync(title);
+    res.send(post);
+  } else {
+    res.status(404)
+    res.send("This post does not exist!")
+  }
+}
+)
 app.listen(3000)
+
