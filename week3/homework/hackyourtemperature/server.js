e4457156920570a231b9f0a3e463826259a9f1be
Nizami Mursudlu <nizamimursudlu@gmail.com>
2021-02-10 17:06:54 +0100

    project finished

--- week3/homework/hackyourtemperature/server.js
+++ week3/homework/hackyourtemperature/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const axios = require("axios");
+const { json, response } = require("express");
 
 
 
@@ -18,9 +19,16 @@ app.get('/', (req, res) => {
 
 app.post('/weather', (req, res) => {
 
-  let submitCity = req.body.cityName
-  res.send(submitCity)
+  const API_KEY = require('./sources/keys.json').API_KEY;
+  const submitCity = req.body.cityName
 
+  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${submitCity}&units=metric&appid=${API_KEY}`)
+    .then(response => {
+      const temp = response.data.main.temp
+      res.render('index', { weatherText: `The temperature in ${submitCity} is ${temp}°C!` })
+    })
+    .catch(err => {
+      res.render('index', { weatherText: "City is not found!" })
+    })
 })
-
 app.listen(port)
